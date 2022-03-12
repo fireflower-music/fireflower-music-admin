276e6b19efce9cae2b2f2e1636e64a08e4606bae
lindseyxcdh <822466858@qq.com>
2022-03-12 09:17:29 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+import App from './App.vue'
+
+const myApp = createApp(App)
+
+console.log(import.meta.env.VITE_API_HOST);
+
+myApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
+
+myApp.mount('#app')
