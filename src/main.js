04d2bf39667a41c2ba1bae888ed0eda7326b65e4
lindseyxcdh <822466858@qq.com>
2022-03-16 23:11:20 +0800

    Add Login,ESLint

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
-import { createApp } from 'vue'
-import { Quasar } from 'quasar'
+import { createApp } from 'vue';
+import { Quasar } from 'quasar';
 
 // Import icon libraries
-import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons/material-icons.css';
 
 // Import Quasar css
-import 'quasar/src/css/index.sass'
+import 'quasar/src/css/index.sass';
 
-import App from './App.vue'
+import App from './App.vue';
+import router from './routers/index.js';
 
-const myApp = createApp(App)
+const myApp = createApp(App);
 
-console.log(import.meta.env.VITE_API_HOST);
+// console.log(import.meta.env.VITE_API_HOST);
+
+myApp.use(router);
 
 myApp.use(Quasar, {
-    plugins: {}, // import Quasar plugins and add here
-})
+  plugins: {} // import Quasar plugins and add here
+});
 
-myApp.mount('#app')
+myApp.mount('#app');
