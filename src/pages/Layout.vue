3e9568f6005e34055855f6a205731460454242bd
lindseyxcdh <822466858@qq.com>
2022-03-28 23:33:03 +0800

    Add user menus

--- src/pages/Layout.vue
+++ src/pages/Layout.vue
@@ -4,16 +4,31 @@
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> 花火音乐 </q-toolbar-title>
+        <q-toolbar-title> 花火音乐</q-toolbar-title>
 
         <q-space />
-        <q-avatar color="teal" text-color="white">{{
-          nicknameFirstWord
-        }}</q-avatar>
+        <q-avatar color="teal" text-color="white"
+          >{{ nicknameFirstWord }}
+        </q-avatar>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above side="left" bordered>
+      <q-list padding class="text-primary">
+        <q-item
+          clickable
+          v-ripple
+          active-class="menu-active"
+          v-for="menu in menuRoutes"
+          :key="menu.meta.title"
+          :active="menu.name === route.name"
+        >
+          <q-item-section avatar>
+            <q-icon :name="menu.meta.icon" />
+          </q-item-section>
+          <q-item-section>{{ menu.meta.title }}</q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
@@ -25,6 +40,8 @@
 <script>
 import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
+import { useRoute } from 'vue-router';
+import { menuRoutes } from '../routers/index.js';
 
 export default {
   name: 'Layout',
@@ -33,6 +50,10 @@ export default {
 
     const store = useStore();
 
+    const route = useRoute();
+
+    console.log(route.name);
+
     return {
       nicknameFirstWord: computed(
         () => store.getters['user/nicknameFirstWord']
@@ -40,10 +61,17 @@ export default {
       leftDrawerOpen,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
-      }
+      },
+      menuRoutes,
+      route
     };
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.menu-active {
+  color: white !important;
+  background: #f2c037;
+}
+</style>
