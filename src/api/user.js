12010257687dc72639f7f1659c3a9baf39df4947
lindseyxcdh <822466858@qq.com>
2022-03-31 22:49:28 +0800

    Update userinfo page

--- src/api/user.js
+++ src/api/user.js
@@ -3,3 +3,8 @@ import { post } from './request';
 export const login = (username, password) => {
   return post('/login', { username, password });
 };
+
+// TODO: 01:36:29
+export const search = paging => {
+  return get('/user/', paging);
+};
